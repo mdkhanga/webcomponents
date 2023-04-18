cd047295474bc05eb6d741c98464f546daaf5382
mdkhanga <khangaonkar@gmail.com>
2023-04-18 16:29:07 -0700

    rest call to get users and display

--- myfinance/src/pages/index.js
+++ myfinance/src/pages/index.js
@@ -2,13 +2,33 @@ import Layout from "@/components/Layout"
 import Link from 'next/link'
 
 
-export default function HomePage() {
+export default function HomePage({users}) {
+  console.log(users)
   return (
     <Layout> 
-      <h1> Welcome to the myfinance application </h1>
+      <h1> My Users</h1>
+     {
+        users.map(user => (
+          <h2 key={user.username}> hello {user.email}</h2>
+        ))
+
+      }
+      
       <Link href="/about"> About</Link> <br/>
       <Link href="/accounts"> Accounts</Link>
     </Layout>
 
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:8080/v1/user/users')
+  const users = await res.json()
+
+  console.log(users)
+
+  return {
+    props: {users}
+  }
+
 }
