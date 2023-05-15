e08eb1e7c2798eba88aff627e94291352247b423
mdkhanga <khangaonkar@gmail.com>
2023-05-15 15:36:31 -0700

    Account Item

---
+++ myfinance/components/AccountItem.js
@@ -0,0 +1,22 @@
+
+import Link from 'next/link'
+import styles from '@/styles/AccountItem.module.css'
+
+export default function AccountItem({account}) {
+	return (
+		<div className={styles.account}>
+			<div className={styles.info}>
+				<h3> {account.name}</h3>
+				<h4> {account.type}</h4>
+				
+			</div>
+			<div className={styles.link}>
+				<Link className='btn' href={`/accounts/{account.name}`}>
+					 Details
+				</Link>
+			</div>
+		</div>
+		
+	)
+
+}
