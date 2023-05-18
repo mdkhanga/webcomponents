faa9bde7c5cddfa6408ef2f8f65263817f5410c8
mdkhanga <khangaonkar@gmail.com>
2023-05-17 17:56:16 -0700

    accounts page

--- myfinance/src/pages/accounts/index.js
+++ myfinance/src/pages/accounts/index.js
@@ -1,15 +1,38 @@
 import Link from "next/link"
 import Layout from "@/components/Layout"
+import {API_URL} from "@/config/index"
+import AccountItem from "@/components/AccountItem"
 
-export default function AccountsPage() {
+export default function AccountsPage({accounts}) {
     return (
 
-        <Layout>
-            <h1> My Accounts </h1>
-            <Link href="/"> Home</Link>
+    <Layout> 
+      <h1> My Accounts</h1>
 
-        </Layout>
+      {accounts.length === 0 && <h3>You have no accounts. Please create your accounts </h3> }
 
+      {
+        accounts.map(account => (
+          <AccountItem key={account.name} account={account}/>
+        ))
+
+      }
+      
+      <Link href="/about"> About</Link> <br/>
+      <Link href="/accounts"> Accounts</Link>
+    </Layout>
         
     )
 }
+
+export async function getServerSideProps() {
+    const res = await fetch(`${API_URL}/v1/accounts/manoj`)
+    const accounts = await res.json()
+  
+  
+    return {
+      props: {accounts}
+    }
+  
+  }
+  
