6c0ea1bfb49c38dc73927c7275cc281ea4a4cd41
mdkhanga <khangaonkar@gmail.com>
2023-12-29 16:53:08 -0800

    cannot add date on the client side

--- myfinance/src/pages/index.js
+++ myfinance/src/pages/index.js
@@ -5,15 +5,18 @@ import AccountItem from "@/components/AccountItem"
 
 
 export default function HomePage({accounts}) {
+  const t = Date();
+  var total = accounts.reduce((total, account) => total +account.balance,0);
   return (
     <Layout> 
       <h1> My Accounts</h1>
       {
         accounts.map(account => (
           <AccountItem key={account.name} account={account}/>
+    
         ))
       }
-      
+      <h2> Total : {total}</h2>
     </Layout>
 
   )
