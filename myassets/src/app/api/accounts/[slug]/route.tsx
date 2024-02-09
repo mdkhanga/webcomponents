b34db035ea6515ac6bf2adf716abd5ac41b03efe
mdkhanga <khangaonkar@gmail.com>
2024-02-09 15:36:52 -0800

    Invalidate cache

--- myassets/src/app/api/accounts/[slug]/route.tsx
+++ myassets/src/app/api/accounts/[slug]/route.tsx
@@ -1,11 +1,15 @@
 export async function GET(request: Request, { params }) {
 
+	const headers = request.headers;
+
 	console.log(params.slug)
+	const { signal } = new AbortController()
 	const response = await fetch("http://localhost:8080/v1/accounts/{params.slug}",{
 		method: "GET",
 		headers: {
 			"Content-Type": "application/json"
 		}
+		// , signal
 	} );
 	const accounts = await response.json() 
 	console.log(JSON.stringify(accounts))
