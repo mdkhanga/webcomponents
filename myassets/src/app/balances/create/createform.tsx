8e1372fb4778fc8f4df62f4143ec0c9257808e00
mdkhanga <khangaonkar@gmail.com>
2024-04-30 17:29:31 -0700

    create form

---
+++ myassets/src/app/balances/create/createform.tsx
@@ -0,0 +1,18 @@
+
+export default async function CreateBalanceForm(names: string[]) {
+
+
+	return (
+
+		<div>
+
+			
+		</div>
+
+
+	)
+
+
+
+
+}
--- myassets/src/app/balances/create/page.tsx
+++ myassets/src/app/balances/create/page.tsx
@@ -1,11 +1,95 @@
+import styles from '@/app/ui/styles/Common.module.css';
+import { getServerSession } from "next-auth";
+import { useRouter } from 'next/navigation'
 
 
-export default function CreateBalance() {
+async function getAccountNames(username: string) {
+	let url = `http://localhost:8080/v1/accounts/names/${username}`
+	const res = await fetch(url,{	
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		cache: 'no-store' 
+	})
+	
+	if (!res.ok) {
+	  throw new Error('Failed to fetch data')
+	}
+   
+	return await res.json()
+}
+
+
+export default async function CreateBalance() {
+
+	const router = useRouter();
+
+	const session = await getServerSession() ;
+	const username = session?.user?.name ;
+
+	let  names: any[] = [];
+	
+	if (username !== "" && username !== undefined && username != null) {
+		names = await getAccountNames(username);
+	}
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+	
+		// const username = searchParams.get("u") ;
+	
+		// await createAccount({"name" : name, "type" : type, "balance" : balance}, username);
+	
+		// router.push("/assets");
+	}
+	
+	const handleCancel = async (e) => {
+		e.preventDefault();
+	
+		router.push("/assets");
+	}
 
 	return (
 
 		<div>
-			Create new monthly balances
+			 <h1>New Asset </h1>
+		  <form className = {styles.signinform} onSubmit={handleSubmit}>
+			
+			{
+			
+			names.map((name: string) => (
+
+			<div>
+			<label>
+			  {name}:
+			</label>
+			<br/>
+			  <input className={styles.textinput}
+				type="text"
+				value={name}
+				// onChange={(e) => setName(e.target.value)}
+			  />
+			<br />
+			
+			<label>
+			  Type:
+			</label>
+			<br/>  
+
+			</div>
+
+			))
+			}
+
+			<div className={styles.btncontainer}>
+
+				<button className={styles.button} type="button" onClick={handleCancel}>Cancel</button>
+				<button className={styles.button} type="submit">Submit</button>
+				{error && <p style={{ color: 'red' }}>{error}</p>}
+
+			</div>
+		  </form>
 		</div>
 
 	)
